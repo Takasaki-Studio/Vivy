a6d11b1b03f4d83b23d6d9aa6e5267c5cc391859
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2024-09-24 19:28:46 -0300

    Add: append .env

--- src/cli.rs
+++ src/cli.rs
@@ -5,15 +5,21 @@ use clap::Parser;
 #[command(version, about)]
 pub struct Args {
     /// Path to folder project
-    #[arg(short, long)]
+    #[arg(short = 'p', long)]
     pub path: String,
     /// Git url reference
-    #[arg(short, long)]
+    #[arg(short = 'g', long)]
     pub git: String,
     /// Docker envs
-    #[arg(short, long)]
+    #[arg(short = 'e', long)]
     pub env: Vec<String>,
-    /// Docker compose file
+    /// Append envs
+    #[arg(short = 'u', long, default_value_t = false)]
+    pub update_envs: bool,
+    /// Env file location
+    #[arg(short = 'E', long)]
+    pub env_file: Option<String>,
+    /// Docker compose file location
     #[arg(short = 'f', long)]
-    pub docker_compose_file: Option<String>
+    pub docker_compose_file: Option<String>,
 }
--- src/env.rs
+++ src/env.rs
@@ -1,16 +1,67 @@
-use std::fs::{File, remove_file};
-use std::io::Write;
-use std::path::Path;
+use std::fs::{remove_file, File};
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
 
-pub fn create_env(path: &Path, envs: Vec<String>) {
-    let file_path = path.join(".env");
+pub fn create_env(path: &Path, env_file: Option<String>, envs: Vec<String>, update_file: bool) {
+    let file_path = path.join(env_file.unwrap_or_else(|| ".env".to_string()));
 
-    if file_path.exists() {
+    if file_path.exists() && update_file {
+        update_env(file_path, envs);
+
+        println!("Success to update .env");
+        return;
+    } else if file_path.exists() {
         remove_file(&file_path).expect("Fail to remove .env file");
     }
-    
+
     let mut file = File::create(file_path).expect("Fail create .env file");
-    file.write_all(envs.join("\n").as_bytes()).expect("Fail write env");
-    
+    file.write_all(envs.join("\n").as_bytes())
+        .expect("Fail write env");
+
     println!("Success to create .env");
+}
+
+fn update_env(path: PathBuf, envs: Vec<String>) {
+    let mut file = File::open(&path).expect("Fail to open .env file");
+
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)
+        .expect("Fail to read content");
+
+    let old_envs = content.split('\n');
+    let mut new_env = Vec::new();
+
+    for old_env in old_envs {
+        let mut key_value = old_env
+            .split('=')
+            .map(String::from)
+            .collect::<Vec<String>>();
+        let new_value = get_env_value(&envs, &key_value[0]);
+
+        if let Some(value) = new_value {
+            key_value[1] = value;
+        }
+
+        new_env.push(key_value.join("="))
+    }
+
+    remove_file(&path).expect("Fail to remove .env file");
+
+    let mut new_file = File::create(path).expect("Fail to create .env file");
+    new_file
+        .write_all(new_env.join("\n").as_bytes())
+        .expect("Fail to update .env file");
+}
+
+fn get_env_value(envs: &Vec<String>, key: &str) -> Option<String> {
+    for env in envs {
+        let key_value = env.split('=').collect::<Vec<&str>>();
+
+        if key_value[0] == key {
+            return Some(key_value[1].to_string());
+        }
+    }
+
+    None
 }
