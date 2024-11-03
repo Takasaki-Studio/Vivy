e3d82878ea5ffdb0ce704a352a75091d60cecc6c
Yummiii <Leeo05050@protonmail.com>
2024-11-03 12:38:08 -0300

    remove some duplicated fuctions code

--- src/docker.rs
+++ src/docker.rs
@@ -1,47 +1,35 @@
 use std::path::Path;
-use std::process::{Child, Command, Stdio};
+use std::process::{Command, Stdio};
 
-pub fn execute_docker_compose_down(path: &str, file: &Option<String>) {
-    let args = create_args(path, file, vec!["down".to_string()]);
+pub enum ComposeOperations {
+    Up,
+    Down,
+}
 
-    let child: Child = Command::new("docker")
+pub fn execute_docker_compose(path: &str, file: &Option<String>, op: ComposeOperations) {
+    let (args, cmd) = match op {
+        ComposeOperations::Up => (create_args(path, file, vec!["up", "-d", "--build"]), "up"),
+        ComposeOperations::Down => (create_args(path, file, vec!["down"]), "down"),
+    };
+
+    let child = Command::new("docker")
         .args(args)
         .stdout(Stdio::piped())
         .spawn()
-        .expect("Fail docker compose down");
+        .expect("Failed to spawn docker compose");
 
-    child.wait_with_output().expect("Fail docker compose down");
+    child
+        .wait_with_output()
+        .expect("Failed to get docker compose output");
 
-    println!("Success docker compose down");
+    println!("Sucess docker compose {}", cmd);
 }
 
-pub fn execute_docker_compose_up(path: &str, file: &Option<String>) {
-    let args = create_args(
-        path,
-        file,
-        vec!["up".to_string(), "-d".to_string(), "--build".to_string()],
-    );
-
-    let child: Child = Command::new("docker")
-        .args(args)
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("Fail docker compose up");
-
-    child.wait_with_output().expect("Fail docker compose up");
-
-    println!("Success docker compose up");
-}
-
-fn create_args<'a>(
-    path: &'a str,
-    file: &'a Option<String>,
-    mut extra_args: Vec<String>,
-) -> Vec<String> {
+fn create_args(path: &str, file: &Option<String>, extra_args: Vec<&str>) -> Vec<String> {
     let mut args = vec![
-        "compose".to_string(),
-        "--project-directory".to_string(),
-        path.to_string(),
+        "compose".to_owned(),
+        "--project-directory".to_owned(),
+        path.to_owned(),
     ];
 
     if let Some(file) = file {
@@ -51,11 +39,11 @@ fn create_args<'a>(
             .expect("Fail get docker compose file path")
             .to_owned();
 
-        args.push("-f".to_string());
+        args.push("-f".into());
         args.push(file_path);
     }
 
-    args.append(&mut extra_args);
+    args.append(&mut extra_args.into_iter().map(|x| x.to_owned()).collect());
 
     args
 }
